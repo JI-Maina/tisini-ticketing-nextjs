933e3290cc72175e70617966715d91b91e455fff
John Maina <irutujmaina@gmail.com>
2026-03-09 20:53:23 +0300

    added daystar logos

--- types/eventsTickets-d.ts
+++ types/eventsTickets-d.ts
@@ -1,18 +1,3 @@
-type EventTicket = {
-  id: number;
-  name: string;
-  category: string;
-  date: string;
-  venue: string;
-  description: string;
-  image: string | null;
-  max_attendees: string | null;
-  terms_and_conditions: string | null;
-  ticket_types: TicketType[];
-};
-
-type TicketType = { id: number; type: string; price: string };
-
 type TicketEvent = {
   id: string;
   ticket_title: string;
